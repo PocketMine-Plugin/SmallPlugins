eb43efe3c462e7fb99a4b2c94b8f2a559c988c57
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-29 18:10:50 -0700

    Update NickName.php

--- NickName/src/NickName/NickName.php
+++ NickName/src/NickName/NickName.php
@@ -8,49 +8,52 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as NC;
 
-class NickName extends PluginBase{
+class NickName extends PluginBase
+{
 
     public $prefix = NC::GRAY . "» " . NC::AQUA . "NickSystem" . NC::GRAY . " » ";
 
-    public function onEnable()
+    public function onEnable(): void
     {
-        $this->getLogger()->info("NickCommand was loaded!");
+        $this->getLogger()->info("NickCommand has been enabled");
     }
+
+    public function onDisable(): void
+        
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
-        switch ($command->getName()){
+        switch ($command->getName()) {
             case "nickname":
-                if($sender instanceof Player) {
-                    if($sender->hasPermission("nickcommand.nick")) {
-                        if(!empty($args[0])) {
-                            $sender->sendMessage($this->prefix . "Your Nickname was set to: " . NC::GREEN . implode(" ", $args) . NC::GRAY . "!");
+                if ($sender instanceof Player) {
+                    if ($sender->hasPermission("nickcommand.nick")) {
+                        if (!empty($args[0])) {
+                            $sender->sendMessage($this->prefix . "Your nickname has been set to: " . NC::GREEN . implode(" ", $args) . NC::GRAY . "!");
                             $sender->setDisplayName(implode(" ", $args));
-                        }else{
-                            $sender->sendMessage($this->prefix . NC::RED . "Please specify an Nickname!");
+                        } else {
+                            $sender->sendMessage($this->prefix . NC::RED . "Please specify an nickname");
                         }
-                    }else{
-                        $sender->sendMessage($this->prefix . NC::RED . "You dont have the Permission to use this Command!");
+                    } else {
+                        $sender->sendMessage($this->prefix . NC::RED . "You do not have permission to use this command");
                     }
-                }else{
-                    $sender->sendMessage($this->prefix . NC::RED . "This Command is only for Players!");
+                } else {
+                    $sender->sendMessage($this->prefix . NC::RED . "Please use this command in-game");
                 }
                 return true;
-
             case "nick":
-                if($sender instanceof Player) {
-                    if($sender->hasPermission("nickcommand.nick")) {
-                        if(!empty($args[0])) {
-                            $sender->sendMessage($this->prefix . "Your Nickname was set to: " . NC::GREEN . implode(" ", $args) . NC::GRAY . "!");
+                if ($sender instanceof Player) {
+                    if ($sender->hasPermission("nickcommand.nick")) {
+                        if (!empty($args[0])) {
+                            $sender->sendMessage($this->prefix . "Your nickname has been set to: " . NC::GREEN . implode(" ", $args) . NC::GRAY . "!");
                             $sender->setDisplayName(implode(" ", $args));
-                        }else{
-                            $sender->sendMessage($this->prefix . NC::RED . "Please specify an Nickname!");
+                        } else {
+                            $sender->sendMessage($this->prefix . NC::RED . "Please specify an nickname");
                         }
-                    }else{
-                        $sender->sendMessage($this->prefix . NC::RED . "You dont have the Permission to use this Command!");
+                    } else {
+                        $sender->sendMessage($this->prefix . NC::RED . "You do not have permission to use this command");
                     }
-                }else{
-                    $sender->sendMessage($this->prefix . NC::RED . "This Command is only for Players!");
+                } else {
+                    $sender->sendMessage($this->prefix . NC::RED . "Please use this command in-game");
                 }
                 return true;
         }
